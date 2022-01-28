99715abfaaf288d0c0534296c0da65ebee591e38
rcq-rcq <55829293+rcq-rcq@users.noreply.github.com>
2022-01-29 00:35:10 +0100

    posteffect transi

--- GGJ_Duality/Assets/PostEffect/ImageEffectBase.cs
+++ GGJ_Duality/Assets/PostEffect/ImageEffectBase.cs
@@ -3,8 +3,7 @@
 [RequireComponent(typeof(Camera))]
 public class ImageEffectBase : MonoBehaviour
 {
-	[SerializeField]
-	protected Material material;
+	public Material material;
 
 	// OnRenderImage() is called when the camera has finished rendering.
 	protected virtual void OnRenderImage(RenderTexture src, RenderTexture dst)
