9ce93b4bdd58499489d8a8d09e2dbd28039381a0
rcq-rcq <55829293+rcq-rcq@users.noreply.github.com>
2022-01-29 13:28:58 +0100

    transi script

--- GGJ_Duality/Assets/PostEffect/ImageEffectBase.cs
+++ GGJ_Duality/Assets/PostEffect/ImageEffectBase.cs
@@ -5,9 +5,16 @@ public class ImageEffectBase : MonoBehaviour
 {
 	public Material material;
 
+	public float transition;
+
 	// OnRenderImage() is called when the camera has finished rendering.
 	protected virtual void OnRenderImage(RenderTexture src, RenderTexture dst)
 	{
 		Graphics.Blit(src, dst, material);
 	}
+
+    private void Update()
+    {
+		material.SetFloat("_Transi", transition);
+    }
 }
